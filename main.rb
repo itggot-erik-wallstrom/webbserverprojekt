b599ff844b72a0243a7286a10c65a508a6ace799
Erik Wallström <erik.wallstrom@live.com>
2018-02-19 10:43:39 +0100

    Started working on ORM

--- main.rb
+++ main.rb
@@ -2,8 +2,10 @@ require_relative './usermanager.rb'
 
 class Main < Sinatra::Base
 	enable :sessions
+
 	def initialize
-		@usermanager = Usermanager.new
+		db = SQLite3::Database.open('db.sqlite')
+		@usermanager = UserManager.new(db)
 		super
 	end
 
@@ -11,7 +13,43 @@ class Main < Sinatra::Base
 		"Hello World"
 	end
 
+	get '/users/new' do
+		slim :register
+	end
+
+	post '/users' do
+		username = params['username']
+		password = params['password']
+		confirm_password = params['confirm_password']
+
+		if(password != confirm_password)
+			return "Passwords do not match"
+		end
+
+		begin
+			@usermanager.add(username, password)
+			"User creation was successful"
+		rescue Exception => e
+			e.message
+		end
+	end
+
 	get '/users' do
-		usermanager.get_all
+		result = ""
+		users = @usermanager.get_all
+		users.each do | user |
+			result += "#{user[0]}<br>"
+		end
+
+		result
+	end
+
+	get '/users/:id' do
+		begin 
+			user = @usermanager.get_with_id(params[:id])
+			user[0]
+		rescue Exception => e
+			e.message
+		end
 	end
 end
--- usermanager.rb
+++ usermanager.rb
@@ -1,13 +1,56 @@
 class UserManager
-	def initialize
-		@db = SQLite3::DataBase.open('db/db.sqlite')
+	def initialize(db)
+		@db = db
+	end
+
+	def user_exists(options)
+		if(options[:id])
+			result = @db.execute(
+				'SELECT * FROM users WHERE id IS ?', 
+				options[:id]
+			)
+
+			if(result.size == 0)
+				return false
+			end
+		end
+
+		if(options[:name])
+			result = @db.execute(
+				'SELECT * FROM users WHERE name IS ?', 
+				options[:name]
+			)
+			
+			if(result.size == 0)
+				return false
+			end
+		end
+
+		true
 	end
 
 	def add(username, password)
-		
+		hash = BCrypt::Password.create(password)
+		if(user_exists(name: username))
+			raise 'Username already exists'
+		end
+
+		@db.execute(
+			'INSERT INTO users (name, password) VALUES (?, ?)', 
+			username, 
+			hash
+		)
 	end
 
 	def get_all
-		
+		@db.execute('SELECT name FROM users')
+	end
+
+	def get_with_id(id)
+		if(!user_exists(id: id))
+			raise "User does not exist"
+		end
+
+		@db.execute('SELECT name FROM users WHERE id IS ?', id)
 	end
 end
